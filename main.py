5531abfc5ab0f4ca542c5b4b3c8655d5a3d8ba0f
Nicholas Currault <nicholas.curr+dev@gmail.com>
2019-09-11 21:38:53 -0700

    more work on sticker adding interface, untested

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram.error import TelegramError, Unauthorized
+from enum import Enum
 import logging
 import hashlib
 import os
@@ -27,7 +28,17 @@ def generate_forward(msg):
 
     return fname
 
+class UserState(Enum):
+    AWAITING_FORWARD = 1
+    AWAITING_EMOJI = 2
+    AWAITING_PACK = 3
+
 def attempt_pop_from_forward_queue(bot, update, user_data):
+    if "state" in user_data.keys() and user_data["state"] != UserState.AWAITING_FORWARD:
+        return
+    else:
+        user_data["state"] = UserState.AWAITING_FORWARD
+
     if len( user_data.get("forward_queue", []) ) == 0:
         update.message.reply_text("Error: forward queue is empty")
     else:
@@ -36,6 +47,7 @@ def attempt_pop_from_forward_queue(bot, update, user_data):
         try:
             with open(fname, "rb") as f:
                 bot.send_photo(chat_id=update.message.from_user.id, photo=f, caption="sticker preview")
+                user_data["state"] = UserState.AWAITING_EMOJI
         except Unauthorized as e:
             return update.message.reply_text("Error sending DM! Message @{} to finish creating this forward".format(bot.username))
 
@@ -50,7 +62,31 @@ def forward_handler(bot, update, user_data):
     attempt_pop_from_forward_queue(bot, update, user_data)
 
 def message_handler(bot, update, user_data):
-    pass
+    if "state" not in user_data:
+        return
+    if not update.message.chat.id != update.from_user.id:
+        return # not a DM, should be ignored
+
+    if user_data["state"] == UserState.AWAITING_EMOJI:
+        user_data["pending_emoji"] = update.message.text
+        user_data["state"] = UserState.AWAITING_PACK
+
+    elif user_data["state"] == UserState.AWAITING_PACK:
+        fname = user_data["forward_queue"][0]
+
+        pack_name = update.message.text
+        pack_name += "_by_" + bot.username
+
+        # TODO verify that pack exists, look up owner
+
+        with open(fname, "rb") as f:
+            bot.add_sticker_to_set(TODO_OWNER, pack_name, f, user_data["pending_emoji"])
+            user_data["forward_queue"].pop(0)
+
+
+def cancel_handler(bot, update, user_data):
+    if user_data.get("forward_queue", [])
+        user_data["forward_queue"].pop(0)
 
 if __name__ == "__main__":
     updater = Updater(token=API_KEY)
@@ -59,7 +95,7 @@ if __name__ == "__main__":
     dispatcher.add_handler(CommandHandler("start", start_handler, pass_user_data=True))
 
     dispatcher.add_handler(MessageHandler(Filters.text & Filters.forwarded, forward_handler, pass_user_data=True))
-    dispatcher.add_handler(MessageHandler(Filters.text & (~ Filters.forwarded), message_handler, pass_user_data=True))
+    dispatcher.add_handler(MessageHandler(Filters.text & (~ Filters.forwarded) & (~ Filters.command), message_handler, pass_user_data=True))
 
     # allows viewing of exceptions
     logging.basicConfig(
